7fa190ae75a3bd4482fc42581bc6e3ba5932171f
Olaniyan Temitope <olaniyantemitope69@gmail.com>
2025-01-31 14:12:57 +0100

    new commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const https = require("https");
 const mongoose = require("mongoose");
 
 const port = process.env.PORT || 5000;
@@ -47,8 +48,6 @@ main()
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-const https = require("https");
 
 const params = JSON.stringify({
   email: "customer@email.com",
