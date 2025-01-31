f345bc193d4634cf09b56fa2e59577c7515ab952
Olaniyan Temitope <olaniyantemitope69@gmail.com>
2025-01-31 13:18:36 +0100

    new commit

--- server.js
+++ server.js
@@ -47,3 +47,41 @@ main()
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const https = require("https");
+
+const params = JSON.stringify({
+  email: "customer@email.com",
+  amount: "500000",
+});
+
+const options = {
+  hostname: "api.paystack.co",
+  port: 443,
+  path: "/transaction/initialize",
+  method: "POST",
+  headers: {
+    Authorization: "Bearer sk_test_e7e1cf6224bda73d75948a50852ebd144a61cf01",
+    "Content-Type": "application/json",
+  },
+};
+
+const req = https
+  .request(options, (res) => {
+    let data = "";
+
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      console.log(JSON.parse(data));
+    });
+  })
+  .on("error", (error) => {
+    console.error(error);
+  });
+
+req.write(params);
+
+req.end();
